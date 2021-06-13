fca85af6280a23abf4f3804d49f8205f269704e7
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-06-13 14:35:48 -0500

    added styles

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -20,11 +20,26 @@ class Directory extends Component {
     this.setState({ selectedCampsite: campsite });
   }
 
+  renderSelectedCampsite(campsite) {
+    if (campsite) {
+      return (
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    }
+    return <div />;
+  }
+
   render() {
     const directory = this.props.campsites.map((campsite) => {
       return (
         <div key={campsite.id} className="col-md-5 m-1">
-          <Card>
+          <Card onClick={() => this.onCampsiteSelect(campsite)}>
             <CardImg width="100%" src={campsite.image} alt="campsite.name" />
             <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
@@ -37,6 +52,11 @@ class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
+        <div className="row">
+          <div className="col-md-5 m-1">
+            {this.renderSelectedCampsite(this.state.selectedCampsite)}
+          </div>
+        </div>
       </div>
     );
   }
