f0849c08672f3e3bcda5c140d4b5f309a7035f78
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-06-22 07:22:34 -0500

    updated camsiteinfo

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function Contact(props) {
   return (
     <div className="container">
       <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+        </Breadcrumb>
         <div className="col">
           <h2>Contact Us</h2>
           <hr />
